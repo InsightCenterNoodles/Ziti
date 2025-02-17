8b498614b453a1206678647826ab3d55b23b7459
Nicholas Brunhart-Lupo <nicholasbl@icloud.com>
2025-02-17 08:49:52 -0700

    Temporarily fix reused window state

--- Ziti/ZitiApp.swift
+++ Ziti/ZitiApp.swift
@@ -25,21 +25,25 @@ struct ZitiApp: App {
         
         WindowGroup(id: "noodles_content_window_small", for: NewNoodles.self) {
             $nn in
-            WindowView(new: nn!, scale: .init(x: 0.5, y: 0.375, z: 0.5))
+            // we create new identifiers here to force each new open window to start in fresh state
+            let new_nn = NewNoodles(hostname: nn?.hostname ?? "localhost")
+            WindowView(new: new_nn, scale: .init(x: 0.5, y: 0.375, z: 0.5))
         }
         .windowStyle(.volumetric)
         .defaultSize(width: 0.5, height: 0.375, depth: 0.5, in: .meters)
         
         WindowGroup(id: "noodles_content_window", for: NewNoodles.self) {
             $nn in
-            WindowView(new: nn!, scale: .init(x: 1.0, y: 0.75, z: 1.0))
+            let new_nn = NewNoodles(hostname: nn?.hostname ?? "localhost")
+            WindowView(new: new_nn, scale: .init(x: 1.0, y: 0.75, z: 1.0))
         }
         .windowStyle(.volumetric)
         .defaultSize(width: 1, height: 0.75, depth: 1, in: .meters)
 
         ImmersiveSpace(id: "noodles_immersive_space", for: NewNoodles.self) {
             $nn in
-            NooImmersiveView(new: nn!)
+            let new_nn = NewNoodles(hostname: nn?.hostname ?? "localhost")
+            NooImmersiveView(new: new_nn)
         }.immersionStyle(selection: $current_style, in: .mixed)
     }
 }
